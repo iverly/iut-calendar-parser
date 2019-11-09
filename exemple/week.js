4568d7456c4b97ed8f28ebf43a94949bc35c6a5c
iverly <iverly.dev@gmail.com>
2019-11-09 21:33:00 +0100

    change: add specific date for week v2

--- exemple/week.js
+++ exemple/week.js
@@ -3,7 +3,7 @@ const publisher = require('../index').publisher;
 publisher.getData('https://edt.iut-tlse3.fr/planning/info/g8659.xml')
     .then(
         data => {
-            let identifier = publisher.getWeekIdentifier(data, new Date("11/22/2019"));
+            let identifier = publisher.getWeekIdentifier(data, new Date());
             console.log(publisher.getWeek(data, identifier))
             identifier = publisher.getWeekIdentifier(data, new Date("11/22/2019"));
             console.log(publisher.getWeek(data, identifier))
