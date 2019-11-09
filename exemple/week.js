519b0cdafb2f3b0d59654c405eab71841cc79baa
iverly <iverly.dev@gmail.com>
2019-11-09 21:21:16 +0100

    change: can choose specific date for week identifier

--- exemple/week.js
+++ exemple/week.js
@@ -3,7 +3,7 @@ const publisher = require('../index').publisher;
 publisher.getData('https://edt.iut-tlse3.fr/planning/info/g8659.xml')
     .then(
         data => {
-            const identifier = publisher.getWeekIdentifier(data);
+            const identifier = publisher.getWeekIdentifier(data, new Date());
             console.log(publisher.getWeek(data, identifier))
         }
     );
--- src/publisher.js
+++ src/publisher.js
@@ -8,11 +8,12 @@ async function getData(url) {
     return xml.xml2js(res, {compact: true});
 }
 
-function getWeekIdentifier(data) {
-    const mondayDate = shared.getMonday(new Date());
+function getWeekIdentifier(data, date) {
+    const mondayDate = shared.getMonday(date);
     const mondayDateString = dateFormat(mondayDate, 'dd/mm/yyyy');
     let identifierObj = data.timetable.span.filter(elem => elem._attributes.date == mondayDateString)[0];
-    return identifierObj.alleventweeks._text;
+    if (identifierObj) return identifierObj.alleventweeks._text;
+    return null;
 }
 
 function getWeek(data, identifier) {
